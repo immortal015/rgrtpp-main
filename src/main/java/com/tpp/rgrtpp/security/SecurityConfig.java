33655b73ebd7da9f137b0a52fd9ef87b45aec4b9
immortal015 <69595740+immortal015@users.noreply.github.com>
2024-12-27 01:40:15 +0200

    Rgr1

--- src/main/java/com/tpp/rgrtpp/security/SecurityConfig.java
+++ src/main/java/com/tpp/rgrtpp/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.tpp.rgrtpp.repository;
+package com.tpp.rgrtpp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
